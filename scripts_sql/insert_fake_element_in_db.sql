cb5e043b9409cd12bbc0fe567c4c891e2e9266fc
matheus1714 <matheussilvamartins1714@gmail.com>
2022-04-22 19:05:45 -0300

    fake data

---
+++ scripts_sql/insert_fake_element_in_db.sql
@@ -0,0 +1,20 @@
+INSERT INTO INFRAESTRUTURA VALUES(
+    1,
+    'my_cluster_1',
+    1,
+    'host example1'
+);
+
+INSERT INTO INFRAESTRUTURA VALUES(
+    2,
+    'my_cluster_2',
+    1,
+    'host example2'
+);
+
+INSERT INTO INFRAESTRUTURA VALUES(
+    3,
+    'my_cluster_3',
+    1,
+    'host example3'
+);
