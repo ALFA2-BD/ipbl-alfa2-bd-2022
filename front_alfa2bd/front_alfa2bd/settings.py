8456a7d6477454dfbaad2b6d22898b2faa6cc69a
matheus1714 <matheussilvamartins1714@gmail.com>
2022-04-23 11:58:47 -0300

    add login app

--- front_alfa2bd/front_alfa2bd/settings.py
+++ front_alfa2bd/front_alfa2bd/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'testdb',
+    'login',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -134,3 +135,6 @@ STATICFILES_DIRS = [
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
