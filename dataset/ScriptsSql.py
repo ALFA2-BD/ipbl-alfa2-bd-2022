85ac615c2b73034f683ad352fc2ec27ae47eb8ad
matheus1714 <matheussilvamartins1714@gmail.com>
2022-04-27 19:37:34 -0300

    update Scripts to dotenv struct

--- dataset/ScriptsSql.py
+++ dataset/ScriptsSql.py
@@ -1,17 +1,20 @@
 import psycopg2
-from decouple import config
+import os
+from dotenv import load_dotenv
 
 class ScriptsSql:
     def __init__(self) -> None:
 
         super().__init__()
 
+        load_dotenv()
+
         self.conn = psycopg2.connect(
-            host = config('DB_HOST_NAME'),
-            dbname = config('DB_NAME'),
-            user = config('BD_USER_NAME'),
-            password = config('DB_PASSWORD'),
-            port = config('DB_PORT', cast=int)
+            host = os.getenv('DB_HOST_NAME'),
+            dbname = os.getenv('DB_NAME'),
+            user = os.getenv('BD_USER_NAME'),
+            password = os.getenv('DB_PASSWORD'),
+            port = os.getenv('DB_PORT')
         )
 
     def get_script_sql(self, file_name:str)->None:
@@ -48,10 +51,10 @@ class ScriptsSql:
     def delete_all_data(self)->None:
         try:
             cursor = self.conn.cursor()
-            cursor.execute('DELETE FROM INFRAESTRUTURA')
-            cursor.execute('DELETE FROM UNIDADE_ESCOLAR')
             cursor.execute('DELETE FROM CONTRATO')
             cursor.execute('DELETE FROM NODE')
+            cursor.execute('DELETE FROM INFRAESTRUTURA')
+            cursor.execute('DELETE FROM UNIDADE_ESCOLAR')
 
             cursor.execute('DELETE FROM ALUNO')
             cursor.execute('DELETE FROM PROFESSOR')
