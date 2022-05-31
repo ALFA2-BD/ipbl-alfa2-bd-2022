40e20032728b384f6f0b8343a3dc3e134a5290f7
matheus1714 <matheussilvamartins1714@gmail.com>
2022-05-31 18:45:05 -0300

    config routes and data for new struct data

--- apps/professor/views.py
+++ apps/professor/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.template import loader
-from django.urls import reverse
 from django.shortcuts import redirect
 from dataset.ScriptsMongoDB import ScriptsMongoDB
 from utils.DictHelper import DictHelper
+from utils.CryptoHelper import CryptoHelper
+from bson.objectid import ObjectId
 import json
 
 def login(request):
@@ -16,6 +16,8 @@ def login(request):
 
 def verify_login(request):
 
+    crypto = CryptoHelper()
+
     if request.POST:
 
         scripts_mongodb = ScriptsMongoDB()
@@ -24,22 +26,31 @@ def verify_login(request):
         identificador_professor = data['identificador_professor']
         senha_professor = data['senha_professor']
 
-        professor = scripts_mongodb.get_data_find(
+        professores = scripts_mongodb.get_data_find(
             collection_name='professores',
             filter = {
                 'identificador': identificador_professor
             }
-        )[0]
+        )
 
         scripts_mongodb.close_connection()
 
-        if senha_professor == professor['senha']:
+        if len(professores):
 
-            context = {
-                'segment': 'home',
-                'err':''
-            }
-            return redirect('/professor/home', context)
+            professor = professores[0]
+
+            if senha_professor == crypto.decrypt_message(professor['hash_senha']):
+
+                context = {
+                    'segment': 'home',
+                    'err':''
+                }
+
+                response = redirect('/professor/home', context)
+
+                response.set_cookie('identificador', professor['identificador'])
+
+                return response
 
     context = {
         'segment': 'login',
@@ -49,14 +60,46 @@ def verify_login(request):
     return redirect('/professor/login', context)
 
 def home(request):
-    context = {'segment': 'home'}
+    context = {
+        'segment': 'home'
+    }
+
+    identificador = request.COOKIES.get('identificador')
+
     html_template = loader.get_template('professor/screens/home.html')
+
+    response = HttpResponse(html_template.render(context, request))
+    response.set_cookie('identificador', identificador)
+
     return HttpResponse(html_template.render(context, request))
 
 def coleta(request):
-    context = {'segment': 'coleta'}
+
+    identificador = request.COOKIES.get('identificador')
+
+    id_aluno  = request.POST['id_aluno']
+
+    scripts_mongodb = ScriptsMongoDB()
+
+    aluno = scripts_mongodb.db['alunos'].find_one(ObjectId(id_aluno))
+    professor = scripts_mongodb.db['professores'].find_one({'identificador': identificador})
+
+    scripts_mongodb.close_connection()
+
+    context = {
+        'segment': 'coleta',
+        'professor': professor,
+        'aluno': aluno
+    }
+
+    identificador = request.COOKIES.get('identificador')
+
     html_template = loader.get_template('professor/screens/coleta.html')
-    return HttpResponse(html_template.render(context, request))
+
+    response = HttpResponse(html_template.render(context, request))
+    response.set_cookie('identificador', identificador)
+
+    return response
 
 def banco_frases(request):
 
@@ -72,15 +115,21 @@ def banco_frases(request):
     }
 
     html_template = loader.get_template('professor/screens/banco_frases.html')
-    return HttpResponse(html_template.render(context, request))
+    identificador = request.COOKIES.get('identificador')
+
+    response = HttpResponse(html_template.render(context, request))
+    response.set_cookie('identificador', identificador)
+    return response
 
 def informacoes(request):
+
+    identificador = request.COOKIES.get('identificador')
 
     scripts_mongodb = ScriptsMongoDB()
 
     professor = scripts_mongodb.get_data_find(
         collection_name='professores',
-        filter = {'cpf': '123.870.496-40'}
+        filter = {'identificador': identificador}
     )[0]
 
     scripts_mongodb.close_connection()
@@ -91,23 +140,38 @@ def informacoes(request):
     }
 
     html_template = loader.get_template('professor/screens/informacoes.html')
-    return HttpResponse(html_template.render(context, request))
+
+    response = HttpResponse(html_template.render(context, request))
+
+    response.set_cookie('identificador', identificador)
+
+    return response
 
 def turmas(request):
+
+    identificador = request.COOKIES.get('identificador')
 
     scripts_mongodb = ScriptsMongoDB()
 
     professor = scripts_mongodb.get_data_find(
         collection_name='professores',
-        filter = {'cpf': '123.870.496-40'}
+        filter = {
+            'identificador': identificador
+        }
     )[0]
 
-    turmas = [] if 'turmas' not in professor else professor['turmas']
+    turmas = scripts_mongodb.get_data_find(
+        collection_name='turmas',
+        filter = {
+            "professor": professor
+        }
+    )
 
     scripts_mongodb.close_connection()
 
     for i in range(len(turmas)):
         turmas[i]['quant_alu'] = len(turmas[i]['alunos'])
+        turmas[i]['id_turma'] = str(turmas[i].get('_id'))
 
     context = {
         'segment': 'turmas',
@@ -115,22 +179,42 @@ def turmas(request):
     }
 
     html_template = loader.get_template('professor/screens/turmas.html')
-    return HttpResponse(html_template.render(context, request))
+    response = HttpResponse(html_template.render(context, request))
+    response.set_cookie('identificador', identificador)
+    return response
 
 def alunos(request):
 
+    identificador = request.COOKIES.get('identificador')
+
     if request.POST:
 
-        alunos = request.POST['alunos']
+        id_turma = request.POST['id_turma']
+
+        scripts_mongodb = ScriptsMongoDB()
+
+        turma = scripts_mongodb.db['turmas'].find_one(ObjectId(id_turma))
+
+        scripts_mongodb.close_connection()
+
+        alunos = turma['alunos']
+
+        for i in range(len(alunos)):
+            alunos[i]['id_aluno'] = str(alunos[i].get('_id'))
 
         context = {
             'segment': 'alunos',
-            'alunos': json.loads(alunos.replace("\'", '\"'))
+            'alunos': alunos
         }
+
         html_template = loader.get_template('professor/screens/alunos.html')
-        return HttpResponse(html_template.render(context, request))
+        response = HttpResponse(html_template.render(context, request))
+        response.set_cookie('identificador', identificador)
+        return response
 
     context = {
         'segment': 'home',
     }
-    return redirect('/professor/home', context)
+    response = redirect('/professor/home', context)
+    response.set_cookie('identificador', identificador)
+    return response
